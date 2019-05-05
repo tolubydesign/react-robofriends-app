54c718ff60791d0d23450bd20a4b5e433d5fbb43
Tolulope Oyegbola Adesina <tolu.adesina79@gmail.com>
2019-05-05 19:25:46 +0200

    update: logic to show user cards

--- src/App.js
+++ src/App.js
@@ -15,9 +15,18 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response => 
+      response.json()
+    ).then( users => 
+      this.setState({ robots: users })
+    );
   };
 
   onSearchChange = (event) => {
@@ -32,15 +41,20 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return (
-      <div className="tc">
-        <h1 className="f1"> RoboFriends </h1>
-        <SearchBox searchChange={ this.onSearchChange }></SearchBox>
-        { /* on try ( 1 ) */ }
-        {/* <CardList robots={ this.state.robots }></CardList> */}
-        <CardList robots={ filteredRobots }></CardList>
-      </div>
-    );
+
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {      
+      return (
+        <div className="tc">
+          <h1 className="f1"> RoboFriends </h1>
+          <SearchBox searchChange={ this.onSearchChange }></SearchBox>
+          { /* on try ( 1 ) */ }
+          {/* <CardList robots={ this.state.robots }></CardList> */}
+          <CardList robots={ filteredRobots }></CardList>
+        </div>
+      );
+    }
   }
 }
 
