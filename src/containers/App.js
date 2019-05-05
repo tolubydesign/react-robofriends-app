09da626ce898af0f1e4da3d6fd55470c58a80761
Tolulope Oyegbola Adesina <tolu.adesina79@gmail.com>
2019-05-05 20:25:50 +0200

    update: improved app folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 // component
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 
 // data 
-import { robots } from './robots';
+import { robots } from '../robots';
 
 // css 
 import './App.css';
@@ -39,14 +39,14 @@ class App extends Component {
   };
 
   render () {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
 
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>
-    } else {      
-      return (
+    return !robots.length ?
+      <h1>Loading</h1> :
+     (
         <div className="tc">
           <h1 className="f1"> RoboFriends </h1>
           <SearchBox searchChange={ this.onSearchChange }></SearchBox>
@@ -59,6 +59,5 @@ class App extends Component {
       );
     }
   }
-}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 // components
-import App from './App'
+import App from './containers/App'
 
 // utils
 import 'tachyons';
