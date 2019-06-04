6cb6b284c7d53d0871088f1424ee934d79cc8217
Tolu Adesina <tolu.adesina79@gmail.com>
2019-06-04 22:33:01 +0200

    add: redux Store and Provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 // components
-import App from './containers/App'
+import App from './containers/App';
+import { searchRobots } from './reducers';
 
 // utils
 import 'tachyons';
 
+const store = createStore(searchRobots)
 
-
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
